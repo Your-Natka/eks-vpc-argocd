27c9647d89b9881651abb436c58287f576155b3a
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2026-09-06 17:30:36 +0200

    fix: increase EKS cpu node size and set AWS profile

--- eks/main.tf
+++ eks/main.tf
@@ -33,7 +33,7 @@ module "eks" {
     cpu-nodes = {
       name = "cpu-nodes"
 
-      instance_types = ["t3.micro"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 1
